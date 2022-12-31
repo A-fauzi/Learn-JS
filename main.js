4037b78d48425f068fcdb60b37b0dcd13e0afdbd
A-fauzi <akhmadfauzi0101@gmail.com>
2022-12-31 17:28:43 +0700

    baru sampe dom

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ import { log } from './logger'
 log('Import works!')
 
 function alert(msg) {
-  alert(msg)
+alert(msg)
 }
 
 function txtChange(name) {
-  // Change text when user click button
+// Change text when user click button
 var getText = document.getElementById('txtChangeConfirmAlert')
 
 getText.innerHTML = `Hello ${name}`
@@ -17,41 +17,59 @@ getText.innerHTML = `Hello ${name}`
 const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
 
 const alertBootstrap = (message, type) => {
-  const wrapper = document.createElement('div')
-  wrapper.innerHTML = [
-    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-    `   <div>${message}</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+const wrapper = document.createElement('div')
+wrapper.innerHTML = [
+`<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    ` <div>${message}</div>`,
+    ' <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
     '</div>'
-  ].join('')
+].join('')
 
-  alertPlaceholder.append(wrapper)
+alertPlaceholder.append(wrapper)
 }
 
 var confrm = confirm("Apakah nama kamu mau di tampilin?");
 
-        if (confrm) {
-            var nama = prompt('Sebutkan nama kamu...', "di rahasiakan!")
-            txtChange(nama)
-            alertBootstrap(`Thankyou ${nama} :)`, 'success')
-        } else {
-            alertBootstrap('Baiklah, kami tidak memaksa!')
-        }
+if (confrm) {
+var nama = prompt('Sebutkan nama kamu...', "di rahasiakan!")
+txtChange(nama)
+alertBootstrap(`Thankyou ${nama} :)`, 'success')
+} else {
+alertBootstrap('Baiklah, kami tidak memaksa!')
+}
 
 
-var counter = 0 
+var counter = 0
 function textCounter() {
-  counter++
-  console.log(counter);
-  
-  document.getElementById('txtCounter').innerHTML = counter
+counter++
+console.log(counter);
+
+document.getElementById('txtCounter').innerHTML = counter
 }
 
 const btnCounter = document.getElementById('btnCounter')
 
 btnCounter.addEventListener('click', () => {
-  console.log('clicked');
- 
-  textCounter()
+console.log('clicked');
 
+textCounter()
+
+})
+
+let countBanana = 0
+let priceBanana = 0
+const btnBanana = document.getElementById('btnBanana')
+btnBanana.addEventListener('click', () => {
+countBanana++
+priceBanana += 200
+document.getElementById('resultShopBanana').innerHTML = `Buy ${countBanana} Pcs | Total: $${priceBanana}`
+})
+
+let countKiwi = 0
+let priceKiwi = 0
+const btnKiwi = document.getElementById('btnKiwi')
+btnKiwi.addEventListener('click', () => {
+countKiwi++
+priceKiwi += 200
+document.getElementById('resultShopKiwi').innerHTML = `Buy ${countKiwi} Pcs | Total: $${priceKiwi}`
 })
